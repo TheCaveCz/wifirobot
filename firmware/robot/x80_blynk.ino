71574fdbd7adf97f92bcf524b9105b590a7f1aaa
Jindrich Dolezy <jindrich.dolezy@strv.com>
2019-04-11 15:50:57 +0200

    Add motor speed pins

--- firmware/robot/x80_blynk.ino
+++ firmware/robot/x80_blynk.ino
@@ -28,6 +28,14 @@ BLYNK_WRITE(BLYNK_RGB_PIN) {
   logicSetLight(param[0].asInt(), param[1].asInt(), param[2].asInt());
 }
 
+BLYNK_WRITE(BLYNK_M1_SPEED_PIN) {
+  m1targetSpeed = constrain(param.asInt(), 0, MOTORS_MAX);
+}
+
+BLYNK_WRITE(BLYNK_M2_SPEED_PIN) {
+  m2targetSpeed = constrain(param.asInt(), 0, MOTORS_MAX);
+}
+
 bool blynkIsConfigured() {
   return strlen(config.blynkToken) != 0;
 }
